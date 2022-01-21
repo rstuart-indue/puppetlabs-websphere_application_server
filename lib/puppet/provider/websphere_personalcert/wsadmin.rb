21c7c73e465d26c04cd9b36d96df20acc9f4c1ac
Bogdan Iamandei <bogdan.iamandei@gmail.com>
2022-01-21 20:10:43 +1000

    First commit for personal cert type Keytool cmd helper

---
+++ lib/puppet/provider/websphere_personalcert/wsadmin.rb
@@ -0,0 +1,185 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'English'
+require_relative '../websphere_helper'
+
+Puppet::Type.type(:websphere_personalcert).provide(:wsadmin, parent: Puppet::Provider::Websphere_Helper) do
+  desc <<-DESC
+    Provider to manage or create an SSL Personal Certificate at a specific scope.
+
+    Please see the IBM documentation available at:
+    https://www.ibm.com/docs/en/was/9.0.5?topic=tool-personalcertificatecommands-command-group-admintask-object
+
+    It is recommended to consult the IBM documentation as the SSL Personal Certificate subject is reasonably complex.
+
+    This provider will not allow the creation of a dummy instance.
+    This provider will not allow the changing of:
+      * the name of the Personal Certificate object.
+    You need to destroy it first, then create another one with the desired attributes.
+
+    We execute the 'wsadmin' tool to query and make changes, which interprets
+    Jython. This means we need to use heredocs to satisfy whitespace sensitivity.
+    DESC
+
+  # We are going to use the flush() method to enact all the changes we may perform.
+  # This will speed up the application of changes, because instead of changing every
+  # attribute individually, we coalesce the changes in one script and execute it once.
+  def initialize(val = {})
+    super(val)
+    @property_flush = {}
+
+    # Dynamic debugging
+    @jython_debug_state = Puppet::Util::Log.level == :debug
+  end
+
+  # This creates the XOR string used in the authentication data entries.
+  # For now, WAS8 and WAS9 are using the same schema of obfuscation for
+  # the alias and KeyStore passwords.
+  # The character used as the XOR key is "_" (underscore).
+  def xor_string (val)
+    xor_result = ""
+    debase64 = Base64.decode64(val)
+
+    debase64.each_char { |char|
+      xor_result += (char.ord ^ "_".ord).ord.chr
+    }
+    return xor_result
+  end
+
+  def scope(what)
+    file = "#{resource[:profile_base]}/#{resource[:dmgr_profile]}"
+
+    # I don't honestly know where the query/mod could be used, but sure as hell
+    # the xml entry is used in security.xml scope attribute for a management scope.
+    # It's yet another way of defining scope in WAS.
+    case resource[:scope]
+    when 'cell'
+      query = "/Cell:#{resource[:cell]}"
+      mod   = "cells/#{resource[:cell]}"
+      xml   = "(cell):#{resource[:cell]}"
+    when 'cluster'
+      query = "/Cell:#{resource[:cell]}/ServerCluster:#{resource[:cluster]}"
+      mod   = "cells/#{resource[:cell]}/clusters/#{resource[:cluster]}"
+      xml   = "(cell):#{resource[:cell]}:(cluster):#{resource[:cluster]}"
+    when 'node'
+      query = "/Cell:#{resource[:cell]}/Node:#{resource[:node_name]}"
+      mod   = "cells/#{resource[:cell]}/nodes/#{resource[:node_name]}"
+      xml   = "(cell):/#{resource[:cell]}:(node):#{resource[:node_name]}"
+    when 'server'
+      query = "/Cell:#{resource[:cell]}/Node:#{resource[:node_name]}/Server:#{resource[:server]}"
+      mod   = "cells/#{resource[:cell]}/nodes/#{resource[:node_name]}/servers/#{resource[:server]}"
+      xml   = "(cell):#{resource[:cell]}:(node):#{resource[:node_name]}:(server):#{resource[:server]}"
+    else
+      raise Puppet::Error, "Unknown scope: #{resource[:scope]}"
+    end
+
+    file += "/config/cells/#{resource[:cell]}/security.xml"
+
+    case what
+    when 'query'
+      query
+    when 'mod'
+      mod
+    when 'xml'
+      xml
+    when 'file'
+      file
+    else
+      debug 'Invalid scope request'
+    end
+  end
+
+  # Create a Personal Certificate Resource
+  def create
+
+  end
+
+  # Check to see if a Personal Certificate exists - must return a boolean.
+  def exists?
+    unless File.exist?(scope('file'))
+      return false
+    end
+
+    debug "Retrieving value of #{resource[:cert_alias]} from #{scope('file')}"
+    doc = REXML::Document.new(File.open(scope('file')))
+
+    sec_entry = XPath.first(doc, "/security:Security]")
+    
+    # This is a lot sillier than I hoped for. Basically the WAS security.xml is a dog's
+    # breakfast with some of its XML functionality excised and its structure flattened.
+    # Instead of using the category/subcategory/sub-subcategory/item structure, they
+    # chose to use the attributes in an item or category to keep track of the parenthood
+    # thereby flattening the XML structure and offloading the XML logic to the application.
+    # So what we are trawling through is something like this:
+    #
+    #  <managementScopes xmi:id="ManagementScope_1" scopeName="(cell):CELL_01" scopeType="cell"/>
+    #  <managementScopes xmi:id="ManagementScope_2" scopeName="(cell):CELL_01:(node):DMGR_01" scopeType="node"/>
+    #  <managementScopes xmi:id="ManagementScope_1623648134948" scopeName="(cell):CELL_01:(node):NODE01" scopeType="node"/>
+    #  <managementScopes xmi:id="ManagementScope_1623648150298" scopeName="(cell):CELL_01:(node):NODE02" scopeType="node"/>
+    #  <keyStores xmi:id="KeyStore_1" name="CellDefaultKeyStore" password="{xor}XORRED_PASSWORD=" provider="IBMJCE" location="${CONFIG_ROOT}/cells/CELL_01/key.p12" type="PKCS12" fileBased="true" hostList="" description="Default key store for CELL_01" usage="SSLKeys" managementScope="ManagementScope_1"/>
+    #  <keyStores xmi:id="KeyStore_2" name="CellDefaultTrustStore" password="{xor}XORRED_PASSWORD=" provider="IBMJCE" location="${CONFIG_ROOT}/cells/CELL_01/trust.p12" type="PKCS12" fileBased="true" hostList="" description="Default trust store for CELL_01" usage="SSLKeys" managementScope="ManagementScope_1"/>
+    #  <keyStores xmi:id="KeyStore_3" name="CellLTPAKeys" password="{xor}XORRED_PASSWORD=" provider="IBMJCE" location="${CONFIG_ROOT}/cells/CELL_01/ltpa.jceks" type="JCEKS" fileBased="true" hostList="" description="LTPA key store for CELL_01" usage="KeySetKeys" managementScope="ManagementScope_1"/>
+    #
+    # Note how "managementScope_1" is the attribute which ties the keyStores item to a managementScopes item.
+    #
+    # Aside to the fact that the managementScopes is there as a work-around for not having multiple security.xml files in
+    # all the right places like we're having for the rest of the WAS settings
+    #
+    # So, with this said, what we have to do here is:
+    #   * Find the appropriate management scope ID by working it back from the scope name (the XML one).
+    #   * Find if we have a keystore with the given name/path in that particular management scope ID
+    #   * Extract the keystore details from the entry attributes if we do find one.
+    
+    # Turns out that the Personal Certificate name has to be unique within the given management scope.
+    mgmt_scope = XPath.first(sec_entry, "managementScopes[@scopeName='#{scope('xml')}']/@*[local-name()='id']") unless sec_entry.nil?
+    debug "Found Management Scope entry for scope '#{resource[:scope]}': #{mgmt_scope.value.to_s}" unless mgmt_scope.nil?
+
+    ks_entry = XPath.first(sec_entry, "keyStores[@managementScope='#{mgmt_scope.value.to_s}'][@name='#{@resource[:key_store_name]}']") unless mgmt_scope.nil?
+
+    debug "Found Keystore entry for scope #{scope('xml')}: #{ks_entry}" unless ks_entry.nil?
+
+    kstore_data = {}
+    XPath.each(ks_entry, "@*[local-name()='password' or local-name()='location' or local-name()='type']") { |attribute|
+      case attribute.name.to_s
+      when 'location'
+        kstore_data[attribute.name.to_sym] = attribute.value.to_s.sub(/${CONFIG_ROOT}/, "#{resource[:profile_base]}/#{resource[:dmgr_profile]}/config")
+      when 'password'
+        # De-obfuscate the target keystore password (should we pass that password in as an attribute?!)
+        kstore_data[attribute.name.to_sym] = xor_string(attribute.value.to_s.match(/^(?:{xor})(.*)/).captures.first)
+      else
+        kstore_data[attribute.name.to_sym] = attribute.value.to_s
+      end
+    } unless ks_entry.nil?
+
+    debug "Exists? method result for #{resource[:key_store_name]} is: #{ks_entry}"
+    keytoolcmd = "-storetype #{kstore_data[:type]} -keystore .#{kstore_data[:location]} -alias #{resource[:cert_alias]}"
+
+    debug "Running command: #{keytoolcmd} as user: #{resource[:user]}"
+    result = keytool(filepass: kstore_data[:password], command: keytoolcmd, failonfail: false)
+    debug result
+  
+    case result
+    when %r{keytool error: java.lang.Exception: Alias <#{resource[:cert_alias]}> does not exist}
+      return :false
+    when %r{keytool error: java.lang.Exception: Keystore file does not exist: #{kstore_data[:location]}}
+      raise Puppet.Error, "Unable to open KeyStore file #{kstore_data[:location]}"
+    when %r{Certificate fingerprint (SHA1): .*}
+      return :true
+    else
+      raise Puppet.Error, "An unexpected error has occured running keytool: #{result}"
+    end
+
+  end
+
+  # Remove a given Personal Certificate
+  def destroy
+
+  end
+
+  def flush
+    # If we haven't got anything to modify, we've got nothing to flush. Otherwise
+    # parse the list of things to do
+    return if @property_flush.empty?
+  end
+end
---
+++ lib/puppet/type/websphere_personalcert.rb
@@ -0,0 +1,240 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+Puppet::Type.newtype(:websphere_personalcert) do
+  @doc = <<-DOC
+    @summary This manages a WebSphere SSL Personal Certificate resource.
+
+    @example
+      websphere_personalcert { 'cert_alias':
+        ensure             => 'present',
+        key_store_name     => 'CellDefaultKeyStore',
+        key_file_path      => '/some/path/to/source-keystore.p12',
+        key_file_pass      => 'SourceKeyStorePassword',
+        key_file_type      => 'PKCS12',
+        key_file_certalias => 'SourceCertAlias'
+        replace_old_cert   => true,
+        delete_old_cert    => true,
+        delete_old_signers => true,
+        profile_base       => '/opt/IBM/WebSphere/AppServer/profiles',
+        dmgr_profile       => 'PROFILE_DMGR_01',
+        cell               => 'CELL_01',
+        user               => 'webadmin',
+        wsadmin_user       => 'wasadmin',
+        wsadmin_pass       => 'password',
+      }
+  DOC
+
+  ensurable
+
+  # Our title_patterns method for mapping titles to namevars for supporting
+  # composite namevars.
+  def self.title_patterns
+    [
+      # CertAlias
+      [
+        %r{^([^:]+)$},
+        [
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:CertAlias
+      [
+        %r{^([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:PROFILE_DMGR_01:CertAlias
+      [
+        %r{^([^:]+):([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:dmgr_profile],
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:PROFILE_DMGR_01:cell:CELL_01:CertAlias
+      [
+        %r{^([^:]+):([^:]+):(cell):([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:dmgr_profile],
+          [:scope],
+          [:cell],
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:PROFILE_DMGR_01:cluster:CELL_01:TEST_CLUSTER_01:CertAlias
+      [
+        %r{^([^:]+):([^:]+):(cluster):([^:]+):([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:dmgr_profile],
+          [:scope],
+          [:cell],
+          [:cluster],
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:PROFILE_DMGR_01:node:CELL_01:AppNode01:CertAlias
+      [
+        %r{^([^:]+):([^:]+):(node):([^:]+):([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:dmgr_profile],
+          [:scope],
+          [:cell],
+          [:node_name],
+          [:cert_alias],
+        ],
+      ],
+      # /opt/IBM/WebSphere/AppServer/profiles:PROFILE_DMGR_01:server:CELL_01:AppNode01:AppServer01:CertAlias
+      [
+        %r{^([^:]+):([^:]+):(server):([^:]+):([^:]+):([^:]+):([^:]+)$},
+        [
+          [:profile_base],
+          [:dmgr_profile],
+          [:scope],
+          [:cell],
+          [:node_name],
+          [:server],
+          [:cert_alias],
+        ],
+      ],
+    ]
+  end
+
+  validate do
+    raise ArgumentError, "Invalid scope #{self[:scope]}: Must be cell, cluster, node, or server" unless %r{^(cell|cluster|node|server)$}.match?(self[:scope])
+    raise ArgumentError, 'server is required when scope is server' if self[:server].nil? && self[:scope] == 'server'
+    raise ArgumentError, 'cell is required' if self[:cell].nil?
+    raise ArgumentError, 'node_name is required when scope is server, or node' if self[:node_name].nil? && self[:scope] =~ %r{(server|node)}
+    raise ArgumentError, 'cluster is required when scope is cluster' if self[:cluster].nil? && self[:scope] =~ %r{^cluster$}
+    raise ArgumentError, "Invalid profile_base #{self[:profile_base]}" unless Pathname.new(self[:profile_base]).absolute?
+    raise ArgumentError, "Invalid key_file_path #{self[:key_file_path]}" unless Pathname.new(self[:key_file_path]).absolute?
+
+    if self[:profile].nil?
+      raise ArgumentError, 'profile is required' unless self[:dmgr_profile]
+      self[:profile] = self[:dmgr_profile]
+    end
+
+    [:cert_alias, :server, :cell, :node_name, :cluster, :profile, :user].each do |value|
+      raise ArgumentError, "Invalid #{value} #{self[:value]}" unless %r{^[-0-9A-Za-z._]+$}.match?(value)
+    end 
+  end
+
+  newparam(:cert_alias) do
+    isnamevar
+    desc <<-EOT
+    Required. The Personal Certificate alias name to create/modify/remove.
+    
+    Example: `www.foo.bar.baz_expiry_YYYYMMDD`
+    EOT
+  end
+
+  newparam(:key_store_name) do
+    desc 'Required. The name of the destination keystore for the import operation.'
+  end
+
+  newparam(:key_file_path) do
+    desc 'Required. The fully qualified path on the filesystem of the source keystore from which to import the personal certificate.'
+  end
+
+  newparam(:key_file_pass) do
+    desc "Required. The password to access the source keystore."
+  end
+
+  newparam(:key_file_type) do
+    newvalues(:PKCS12, :JCEKS, :JKS, :CMSKS, :PKCS11)
+    desc 'Required. The SSL type of the source keystore. One of the following: `PKCS12`, `JCEKS`, `JKS`, `CMSKS`, `PKCS11` '
+  end
+
+  newparam(:key_file_certalias) do
+    desc "Required. The personal certificate alias name to import from the source keystore."
+  end
+
+  newparam(:replace_old_cert) do
+    defaultto :false
+    newvalues(:true, :false)
+    desc 'Optional. Set the value of this parameter to true in order to replace the old destination certificate with the newly imported one. Defaults to `false`'
+  end
+
+  newparam(:delete_old_cert) do
+    defaultto :false
+    newvalues(:true, :false)
+    desc 'Optional. Set the value of this parameter to true in order to delete the old certificates during certificate replacement. Defaults to `false`'
+  end
+
+  newparam(:delete_old_signers) do
+    defaultto :false
+    newvalues(:true, :false)
+    desc 'Optional. Set the value of this parameter to true in order to delete the old signer certificates during certificate replacement. Defaults to `false`'
+  end
+
+  newparam(:scope) do
+    isnamevar
+    desc <<-EOT
+    The scope for the Personal Certificate .
+    Valid values: cell, cluster, node, or server
+    EOT
+  end
+
+  newparam(:server) do
+    isnamevar
+    desc 'The server for which this Personal Certificate should be set'
+  end
+
+  newparam(:cell) do
+    isnamevar
+    desc 'The cell for which this Personal Certificate should be set'
+  end
+
+  newparam(:node_name) do
+    isnamevar
+    desc 'The node name for which this Personal Certificate should be set'
+  end
+
+  newparam(:cluster) do
+    isnamevar
+    desc 'The cluster for which this Personal Certificate should be set'
+  end
+
+  newparam(:profile) do
+    desc "The profile to run 'wsadmin' under"
+  end
+
+  newparam(:dmgr_profile) do
+    isnamevar
+    defaultto { @resource[:profile] }
+    desc <<-EOT
+    The DMGR profile for which this Personal Certificate should be set under.  Basically, where
+    are we finding `wsadmin`
+
+    This is synonymous with the 'profile' parameter.
+
+    Example: dmgrProfile01"
+    EOT
+  end
+
+  newparam(:profile_base) do
+    isnamevar
+    desc "The base directory where profiles are stored.
+      Example: /opt/IBM/WebSphere/AppServer/profiles"
+  end
+
+  newparam(:user) do
+    defaultto 'root'
+    desc "The user to run 'wsadmin' with"
+  end
+
+  newparam(:wsadmin_user) do
+    desc 'The username for wsadmin authentication'
+  end
+
+  newparam(:wsadmin_pass) do
+    desc 'The password for wsadmin authentication'
+  end
+end
